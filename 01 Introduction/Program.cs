0ae772e64279d1f479443f4b880839cded001350
studentColl <ggxaoc@gmail.com>
2025-04-22 10:09:56 +0300

    Добавьте файлы проекта.

---
+++ 01 Introduction/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+/* C#
+ * - Unity
+ * 
+ * 2000 - первый C# как часть .NET 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
