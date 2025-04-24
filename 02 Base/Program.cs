2cc59f35c445dbf8fa1e68491cdf97e3b6f39146
studentColl <ggxaoc@gmail.com>
2025-04-24 14:04:37 +0300

    2

---
+++ 02 Base/Program.cs
@@ -0,0 +1,38 @@
+﻿class Base
+{
+    static int Main()
+    {
+        string UwU = "UwU";
+        Console.WriteLine(UwU);
+        Console.ReadKey();
+        Console.Clear();
+        Console.WriteLine($"{UwU}");
+        Console.WriteLine("{0}{1}", UwU, UwU);
+        double? OwO = Convert.ToDouble(Console.ReadLine());
+        int a = 10, b = 20;
+        int c = ++a * b--;
+
+        if (a < b)
+        {
+        }
+        else if (c > b)
+        {
+        }
+        else
+        {
+        }
+
+        Console.Write(a > b ? "Lol" : "Mem");
+        switch (c)
+        {
+            case 0:
+            case 1:
+                Console.WriteLine();
+                break;
+            default:
+                break;
+        }
+
+        return 0;
+    }
+}
---
+++ 02.5 Calculator1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.CompilerServices;
+
+class Calculator
+{
+    
+    static int Main()
+    {
+        while (true)
+        {
+            string? firstNum;
+            do
+            {
+                firstNum = Console.ReadLine();
+            } while (firstNum == null);
+
+            if (double.TryParse(firstNum, out var number)) break;
+            else
+            {
+                Console.WriteLine("Not a number!");
+            }
+        }
+
+        while (true)
+        {
+            string? operation;
+            do
+            {
+                operation = Console.ReadLine();
+            } while (operation == null);
+
+            bool result = (String operation =>
+            {
+                switch (operation)
+                {
+                    case "-":
+                    case "+":
+                    case "/":
+                    case "*":
+                    case "%":
+                        return false;
+                    default:
+                        return true;
+                }
+            })
+
+            if ((operation) =>
+            {
+                switch (operation)
+                {
+                    case "-":
+                    case "+":
+                    case "/":
+                    case "*":
+                    case "%":
+                        return false;
+                    default:
+                        return true;
+                }
+            });
+
+        }
+
+        
+        
+
+
+        return 0;
+    }
+}
