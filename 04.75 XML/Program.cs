32aee7e7a18a7d9e440046ec38a892a4f1ea845e
studentColl <ggxaoc@gmail.com>
2025-04-29 14:52:07 +0300

    gfdsgdfsdgf

---
+++ 04.75 XML/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        XDocument contactsDoc = new XDocument(
+            new XElement("Contents",
+                new XElement("Contact",
+                    new XElement("Name", "UwUw"),
+                    new XElement("Phone", "88005553535")
+                ),
+                new XElement("Contact",
+                    new XElement("Name", "Eue"),
+                    new XElement("Phone", "666777228337")
+                )
+            )
+        );
+        contactsDoc.Save("C:\\Users\\studentcoll\\Desktop\\UwU\\.NET\\CollegeClass.NET\\contacts.xml");
+    }
+}
---
+++ 04.8 XML Catalog/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        XDocument document = XDocument.Load("C:\\Users\\studentcoll\\Desktop\\UwU\\.NET\\CollegeClass.NET\\xml.xml");
+        var doc = document.Descendants("Products").Descendants("Product");
+
+        var electronics = doc.Where(c => c.Element("Category").Value == "Electronics");
+        foreach ( var item in electronics)
+        {
+            Console.WriteLine(item.Element("Name").Value);
+        }
+        Console.WriteLine();
+
+        var avalible = doc.Where(c => c.Element("Avalible").Value == "true");
+        foreach (var item in avalible)
+        {
+            Console.WriteLine(item.Element("Name").Value);
+        }
+        Console.WriteLine();
+    }
+}
