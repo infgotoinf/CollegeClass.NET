f4a3d98c85c4fb9604ce33e4920ed06a0e8df846
studentColl <ggxaoc@gmail.com>
2025-04-29 11:02:09 +0300

    rtyhg

---
+++ 04.5 Books/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+    public int Pages { get; set; }
+    public string Genre { get; set; }
+    public Book(string title, string author, int year, int pages, string genre)
+    {
+        Title = title;
+        Author = author;
+        Year = year;
+        Pages = pages;
+        Genre = genre;
+    }
+}
+
+class Program
+{
+    static int Main()
+    {
+        var books = new List<Book>()
+        {
+            new Book("How to use Nlohman C++", "Nlohman", 1969, 69, "Programming"),
+            new Book("UwU in the modern word of technoogy", "Mr.UwUwster", 2024, 300, "IT"),
+            new Book("I use Arch btw", "Mr.UwUwster", 2025, 20, "Linux"),
+            new Book("101 reason to hate python", "CoolGuy", 2023, 512, "Programming"),
+            new Book("Cars in de code", "CoolGuy", 2024, 34, "Programming"),
+            new Book("How to hide a corpse", "ParkourStalkesSniperAssasin2283372007", 2024, 51, "Everyday life"),
+            new Book("Most important keyboard shortcuts for vim", "CoolGuy", 2023, 666, "Programming"),
+            new Book("Fready Fazber's in my code!", "Mr.UwUwster", 2025, 34, "Programming"),
+            new Book("How to use NUKE and GUYS in your code", "CoolGuy", 2025, 23, "Programming"),
+            new Book("[]___-[]--__[]-", "ParkourStalkesSniperAssasin2283372007", 2024, 13, "Diary"),
+        };
+
+        var year = books
+            .Where(x => x.Year <= 2023)
+            .Select(x => x.Title);
+        foreach (var i in year)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+
+        var pagesorder = books
+            .OrderBy(x => x.Pages)
+            .Select(x => x.Title);
+        foreach (var i in pagesorder)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+
+        var genres = books.GroupBy(x => x.Genre);
+        foreach (var i in genres)
+        {
+            Console.WriteLine($"Genre: {i.Key}");
+            foreach (var j in i)
+            {
+                Console.WriteLine($"{j.Title} - {j.Author}");
+            }
+        }
+        Console.WriteLine();
+
+        var count = books.Count();
+        Console.WriteLine($"Number of books - {count}");
+
+        var sum = books.Sum(x => x.Pages);
+        Console.WriteLine($"Sum of pages - {sum}");
+
+        var min = books.Min(x => x.Pages);
+        Console.WriteLine($"Min number of pages - {min}");
+
+        var max = books.Max(x => x.Pages);
+        Console.WriteLine($"Max number of pages - {max}");
+
+        var average = books.Average(x => x.Pages);
+        Console.WriteLine($"Average number of pages - {average}\n");
+
+        foreach (var i in genres)
+        {
+            Console.WriteLine($"Genre \"{i.Key}\" has {i.Count()} books");
+        }
+        Console.WriteLine();
+
+        var cool = books
+            .Where(x => x.Author == "CoolGuy")
+            .OrderBy(x => x.Year)
+            .Select(x => x.Title);
+        foreach (var i in cool)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+
+        var first = books.Take(3);
+        var last = books.Skip(count - 3);
+
+        //Не уверен
+        var nodupl = books.Union(books);
+
+        return 0;
+    }
+}
---
+++ 4 - Linq, Lambda/Program.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+/***************************************************************************/
+
+class Departament
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    public Departament(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
+class Employee
+{
+    public int DepartamentId { get; set; }
+    public string Name { get; set; }
+    public Employee(int departamentId, string name)
+    {
+        DepartamentId = departamentId;
+        Name = name;
+    }
+}
+
+
+/***************************************************************************/
+/***************************************************************************/
+
+class Program
+{
+    static int Main()
+    {
+        int[] nums = { 1, 2, 3, 4, 5 };
+
+        var evenNums = nums.Where(x => x % 2 == 0);
+
+        var evenNums2 = from x in nums
+                        where x % 2 == 0
+                        select x;
+
+        string[] names = { "Loh", "Loh21", "Loh3" };
+
+        /***************************************************************************/
+
+        var sortedNames = names.OrderBy(x => x.Length);
+
+        var sortedNames3 = names.OrderByDescending(x => x.Length);
+
+        foreach (var name in sortedNames)
+        {
+            Console.WriteLine(name);
+        }
+
+        var sortedNames2 = from name in names
+                           orderby name.Length
+                           select name;
+
+        /***************************************************************************/
+
+        var squares = nums.Select(x => x * x);
+
+        var squares2 = from x in squares
+                       select x;
+
+        /***************************************************************************/
+
+        int count = nums.Count();
+        int sum = nums.Sum();
+        int min = nums.Min();
+        int max = nums.Max();
+        double average = nums.Average();
+
+        var result = nums
+            .Where(x => x % 2 != 0)
+            .OrderByDescending(x => x)
+            .Select(x => x * x);
+
+        /***************************************************************************/
+
+        var people = new List<Person>()
+        {
+            new Person("Loh", 25),
+            new Person("Loh21", 21),
+            new Person("Loh3", 69),
+            new Person("Loh404", 25)
+        };
+
+        var groupedPeople = people.GroupBy(x => x.Age);
+
+        foreach (var grouped in groupedPeople)
+        {
+            Console.WriteLine($"Years: {grouped.Key}");
+            foreach (var person in grouped)
+            {
+                Console.WriteLine(person.Name);
+            }
+        }
+
+        /***************************************************************************/
+
+        var departaments = new List<Departament>
+        {
+            new Departament(1, "HR"),
+            new Departament(2, "IT")
+        };
+
+        var employees = new List<Employee>
+        {
+            new Employee(1, "Loh3"),
+            new Employee(2, "Loh"),
+            new Employee(1, "Loh21")
+        };
+
+        var employeeDepartaments = departaments.Join(
+            employees,
+            d => d.Id,
+            e => e.DepartamentId,
+            (d, e) => new {DepartamentName = d.Name, EmployeeName = e.Name}
+            );
+
+        foreach (var item in employeeDepartaments)
+        {
+            Console.WriteLine($"{item.EmployeeName} works in {item.DepartamentName}");
+        }
+
+        /***************************************************************************/
+
+        int[] nums1 = { 1, 2, 3, 4, 5 };
+        int[] nums2 = { 4, 5, 6, 7, 8 };
+
+        var nums3 = nums1.Concat(nums2);
+
+        foreach (var i in nums3)
+        {
+            Console.WriteLine(i);
+        }
+
+        var nums4 = nums3.Distinct();
+
+        foreach (var i in nums4)
+        {
+            Console.WriteLine(i);
+        }
+
+        var nums5 = nums1.Union(nums2);
+        var nums6 = nums1.Intersect(nums2);
+        var nums7 = nums1.Except(nums2);
+
+        var nums8 = nums1.Skip(2).Take(2);
+        var nums9 = nums1.SkipWhile(x => x < 2).TakeWhile(x => x < 4);
+        return 0;
+    }
+}
