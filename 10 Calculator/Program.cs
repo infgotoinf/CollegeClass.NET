1a936cc4bd829a6cf9a7fcbfc9b4576c083937ec
studentColl <ggxaoc@gmail.com>
2025-05-20 10:48:04 +0300

    бургеры

---
+++ 10 Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿class Calculator
+{
+
+    static int Main()
+    {
+        try
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+
+            char operation = char.Parse(Console.ReadLine());
+
+            double secondNum = double.Parse(Console.ReadLine());
+
+            double loh;
+            switch (operation)
+            {
+                case '-':
+                    loh = firstNum - secondNum;
+                    break;
+                case '+':
+                    loh = firstNum + secondNum;
+                    break;
+                case '/':
+                    if (secondNum == 0)
+                        throw new DivideByZeroException("Devision by zero.");
+                    loh = firstNum / secondNum;
+                    break;
+                case '*':
+                    loh = firstNum * secondNum;
+                    break;
+                case '%':
+                    if (secondNum == 0)
+                        throw new DivideByZeroException("Devision by zero.");
+                    loh = firstNum % secondNum;
+                    break;
+                default:
+                    throw new Exception();
+            }
+
+            Console.WriteLine(loh);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        return 0;
+    }
+}
