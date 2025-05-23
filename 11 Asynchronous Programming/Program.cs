39efa9fff29e29012f3f58e7c28b884abdc1a00c
studentColl <ggxaoc@gmail.com>
2025-05-23 12:42:18 +0300

    уууу

---
+++ 11 Asynchronous Programming/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Ассинхронное программирование
+ * 
+ * Это подход к написанию кода при котором выполнение операций выносится  из основного потока выполнения программы.
+ * 
+ * Синхронность - это когда опирации поочереди.
+ * Ассинхронность - некоторые операции выполняются параллельно с основным потоком.
+ * 
+ * Можно использовать, когда долгие вычесления, работа с бд, нужно увеличить отзывчивость приложения.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Method();
+    }
+
+    static void Method()
+    {
+        // Ассинхронная операция возвращающая тип Т
+        Task<int> task = Task.FromResult(42);
+    }
+
+    public async Task Method2()
+    {
+        var task1 = Task.Delay(200);
+        var task2 = Task.Delay(1000);
+
+        await Task.WhenAll(task1, task2);
+        Console.WriteLine("ttttttt");
+
+        var a = await Task.WhenAny(task1, task2);
+    }
+}
